8a342510f5e1d4b34e4d2745c66ec756944e1623
isrovi <bagus.isrovi@gmail.com>
2021-09-27 07:09:03 +0700

    add Dropdown

--- src/pages/Transaction.js
+++ src/pages/Transaction.js
@@ -1,17 +1,54 @@
 import React from "react";
 import { Container, Row, Col, Table } from "react-bootstrap";
 import Wow from "../assets/images/bitwow.png";
-import Drop from "../assets/images/drop.png"
+import Drop from "../assets/images/drop.png";
+import User from "../assets/images/usernav.png";
+import Add from "../assets/images/addbook.png";
+import Logout from "../assets/images/redlogout.png";
+import { Nav, NavDropdown } from "react-bootstrap";
 
 export default function Transaction() {
+  const handleSelect = (eventKey) => alert(`selected ${eventKey}`);
+
   return (
     <>
       <Container>
         <Row>
           <Col className="mt-5">
-            <img sticky="top" src={Wow} alt="" />
+            <Nav
+              sticky="top"
+              className="d-flex justify-content-between"
+              activeKey="1"
+              onSelect={handleSelect}
+            >
+              <Nav.Item>
+                <Nav.Link eventKey="1" href="/home">
+                  <img src={Wow} alt="" />
+                </Nav.Link>
+              </Nav.Item>
+              <NavDropdown title={<img src={User} alt="" />} id="nav-dropdown">
+                <NavDropdown.Item>
+                  <img
+                    className="me-2"
+                    style={{ fontSize: "24px" }}
+                    src={Add}
+                    alt=""
+                  />
+                  <span>Add Book</span>
+                </NavDropdown.Item>
+                <NavDropdown.Item>
+                  <img
+                    className="me-2"
+                    style={{ fontSize: "24px" }}
+                    src={Logout}
+                    alt=""
+                  />
+                  <span>Logout</span>
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
             <div className="m-5">
-                <h1 className="my-5">Incoming Transaction</h1>
+              <h1 className="my-5">Incoming Transaction</h1>
               <Table striped borderless hover>
                 <thead>
                   <tr>
@@ -29,10 +66,12 @@ export default function Transaction() {
                     <td>1</td>
                     <td>Radif Ganteng</td>
                     <td>bca.jpg</td>
-                    <td>@26 / Hari</td>
+                    <td>26 / Hari</td>
                     <td>Active</td>
                     <td>Approve</td>
-                    <td><img src={Drop} alt="" /></td>
+                    <td>
+                      <img src={Drop} alt="" />
+                    </td>
                   </tr>
                   <tr>
                     <td>2</td>
@@ -41,9 +80,10 @@ export default function Transaction() {
                     <td>26 / Hari</td>
                     <td>Active</td>
                     <td>Approve</td>
-                    <td className><img src={Drop} alt="" /></td>
+                    <td className>
+                      <img src={Drop} alt="" />
+                    </td>
                   </tr>
-                  
                 </tbody>
               </Table>
             </div>
